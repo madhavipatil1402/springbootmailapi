27004d67088d847fa4144f4f9e651afb665215e8
madhavipatil1402 <madhavipatil1402@gmail.com>
2024-11-30 12:18:49 +0530

    initial commit

---
+++ springbootemailpractice/src/main/java/com/fullstack/controller/EmailController.java
@@ -0,0 +1,28 @@
+package com.fullstack.controller;
+
+import com.fullstack.model.EmailModel;
+import com.fullstack.service.IEmailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+@Slf4j
+public class EmailController {
+    @Autowired
+    private IEmailService emailService;
+
+    @PostMapping("/sendemail")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailModel emailModel){
+        log.info("Sending Email to :"+emailModel.getToEmail());
+        emailService.sendEmail(emailModel);
+        return ResponseEntity.ok("mail send Successfully");
+    }
+
+
+}
---
+++ springbootemailpractice/src/main/java/com/fullstack/service/IEmailService.java
@@ -0,0 +1,7 @@
+package com.fullstack.service;
+
+import com.fullstack.model.EmailModel;
+
+public interface IEmailService {
+    void sendEmail(EmailModel emailModel);
+}
